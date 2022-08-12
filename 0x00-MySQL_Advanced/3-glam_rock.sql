db30bfb43a706938e095146983b58cfea80a30e7
Migdalis <miglepage@gmail.com>
2022-08-11 23:20:23 -0500

    :white_check_mark: Add tasks 1 - 3

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,3 @@
+-- Script that lists all bands with Glam rock as their main style, ranked by their longevity
+SELECT band_name, IFNULL(split, 2020) - formed as lifespan FROM metal_bands
+    WHERE LOCATE('Glam rock', style) <> 0 ORDER BY lifespan DESC;
